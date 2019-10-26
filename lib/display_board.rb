b83ee8aef0f95dbcb5d4965dc0888cc2fa759122
Azfar Mohamed <azfarmah@outlook.com>
2019-10-26 20:46:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(display)
-  puts display[0]+"  |  "+display[1]+"   |   "+display[2]
+  puts display[0]+"   |  "+display[1]+"   |   "+display[2]
   puts"-----------"
   puts display[3]+"  |   "+display[4]+"  |   "+display[5]
   puts"-----------"
